e36f039ae08e285ce95da7765d4f462565da23b9
Jilvan Jr <jilvanj@hotmail.com>
2023-04-21 18:59:58 -0300

    inserindo rota get sem parametros para retornar todas as pessoas

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -20,9 +21,13 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(
-            @PathVariable(value = "id") String id) throws Exception {
+    public Person findById(@PathVariable(value = "id") String id) {
 
         return service.findById(id);
     }
