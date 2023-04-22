0676fe2723d7aa2f240cbc686b0cbfb6e44767b9
Jilvan Jr <jilvanj@hotmail.com>
2023-04-21 21:36:30 -0300

    Implementando PersonEntity e o Repositório com Hibernate e Spring Data JPA

--- src/main/java/br/com/erudio/model/Person.java
+++ src/main/java/br/com/erudio/model/Person.java
@@ -1,15 +1,31 @@
 package br.com.erudio.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+// Estou dizendo que essa classe representa uma tabela do banco
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // quando o nome nao é o mesmo no banco entao declaro aqui
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+
+    // Como o nome é o mesmo no banco entao nao precisa de name
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
 
     public Person() {}
